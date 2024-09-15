d59b39d9537786d5f737b389bf68491c723f1a86
trraxxe <trraxxe@gmail.com>
2024-09-15 18:53:59 +0300

    Fix BubbleSorter

--- src/main/java/ua/trraxxe/algoritms/sorting/BubbleSorter.java
+++ src/main/java/ua/trraxxe/algoritms/sorting/BubbleSorter.java
@@ -7,17 +7,16 @@ public class BubbleSorter implements Sorter {
     public int[] sort(int[] array) {
         int[] result = Arrays.copyOf(array, array.length);
 
-        boolean sorted = false;
+        boolean isSorted = false;
 
-
-        for (int i = 0; i < array.length && !sorted; i++) {
-            sorted = true;
-            for (int j = 0; j < array.length - i - 1; j++) {
+        for (int i = 0; i < result.length && !isSorted; i++) {
+            isSorted = true;
+            for (int j = 0; j < result.length - i - 1; j++) {
                 if (result[j] > result[j + 1]) {
                     int buffer = result[j];
                     result[j] = result[j + 1];
                     result[j + 1] = buffer;
-                    sorted = false;
+                    isSorted = false;
                 }
             }
         }
